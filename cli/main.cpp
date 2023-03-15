a2b43344eadb0d742090e26c376a88e073614fde
OG <og@hiimog.com>
2023-03-14 21:36:35 -0400

    Remove cli project

--- cli/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "SQLiteCpp/SQLiteCpp.h"
-
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- cli/sanity_test.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(Sanity, IsFine) {
-    EXPECT_EQ(1, 1);
-}
